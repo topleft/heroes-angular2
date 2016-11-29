82d66106d314eec1c82d1d3b4e031d85481b5e67
Pete Jeffryes <pete@flowhub.co>
2016-11-29 00:00:14 -0700

    added a service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+
+
 
 @Component({
     selector: 'my-app',
+    providers: [HeroService],
     template:`
       <h1>{{title}}</h1>
       <h2>My Heroes</h2>
@@ -62,28 +67,26 @@ import { Hero } from './hero';
       border-radius: 4px 0 0 4px;
     }
   `]
-
-
 })
-export class AppComponent {
-  title = 'Tour of Heroes'
+
+export class AppComponent implements OnInit {
+
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+  title = 'Tour of Heroes';
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(result => this.heroes = result);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
   onSelect(hero:Hero): void {
     this.selectedHero = hero
   }
 
  }
-
-const HEROES: Hero[] = [
- { id: 11, name: 'Mr. Nice' },
- { id: 12, name: 'Narco' },
- { id: 13, name: 'Bombasto' },
- { id: 14, name: 'Celeritas' },
- { id: 15, name: 'Magneta' },
- { id: 16, name: 'RubberMan' },
- { id: 17, name: 'Dynama' },
- { id: 18, name: 'Dr IQ' },
- { id: 19, name: 'Magma' },
- { id: 20, name: 'Tornado' }
-];
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,3 @@
-// import { NgModule }      from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-//
-// import { AppComponent }  from './app.component';
-//
-// @NgModule({
-//   imports:      [ BrowserModule ],
-//   declarations: [ AppComponent ],
-//   bootstrap:    [ AppComponent ]
-// })
-// export class AppModule { }
-
-
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
